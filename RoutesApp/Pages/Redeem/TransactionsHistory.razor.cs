aeb56add51924181103620ab21a51facd244f140
Nirmal Patel <nirmal.patel@theonetechnologies.co.in>
2020-10-27 20:27:26 +0530

    Updated urls and added some messages

--- RoutesApp/Pages/Redeem/TransactionsHistory.razor.cs
+++ RoutesApp/Pages/Redeem/TransactionsHistory.razor.cs
@@ -12,7 +12,7 @@ namespace RoutesApp.Pages.Redeem
 {
     public partial class TransactionsHistory
     {
-        string spinner = string.Empty, message = string.Empty, officerId = string.Empty, token = string.Empty;
+        string spinner = string.Empty, message = string.Empty, officerId = string.Empty, token = string.Empty, Name = string.Empty;
         AlertMessageType messageType = AlertMessageType.Success;
         List<RedeemHistory> model = new List<RedeemHistory>();
         [CascadingParameter]
@@ -27,22 +27,40 @@ namespace RoutesApp.Pages.Redeem
                     officerId = _id;
                 }
                 var userState = authenticationState.Result;
-            
+                string UserName = userState.User.FindFirst("Name").Value;
+                string[] arrUserName = UserName.Split(' ');
+                if (arrUserName.Length > 1)
+                {
+                    Name = arrUserName[0].Substring(0, 1) + arrUserName[1].Substring(0, 1);
+                }
+                else
+                {
+                    Name = UserName.Substring(0, 2);
+                }
+
                 var result = await Http.GetAsync("/api/coupons/redeem?officerId=" + officerId + "&include=coupon");
                 var responseData = await result.Content.ReadAsStringAsync();
                 var response = JsonConvert.DeserializeObject<RedemptionGetResponse>(responseData);
                 if (response.status == true)
                 {
-                    foreach (var item in response.data)
+                    if(response.data.Count > 0)
                     {
-                        RedeemHistory redeemModel = new RedeemHistory();
-                        redeemModel.RedemptionId = item.RedemptionId;
-                        redeemModel.CreatedAt = item.CreatedAt;
-                        redeemModel.OfficerId = item.OfficerId;
-                        redeemModel.Title = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.Title).FirstOrDefault();
-                        redeemModel.Subtitle = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.Subtitle).FirstOrDefault();
-                        redeemModel.LogoUrl = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.LogoUrl).FirstOrDefault();
-                        model.Add(redeemModel);
+                        foreach (var item in response.data)
+                        {
+                            RedeemHistory redeemModel = new RedeemHistory();
+                            redeemModel.RedemptionId = item.RedemptionId;
+                            redeemModel.CreatedAt = item.CreatedAt;
+                            redeemModel.OfficerId = item.OfficerId;
+                            redeemModel.Title = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.Title).FirstOrDefault();
+                            redeemModel.Subtitle = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.Subtitle).FirstOrDefault();
+                            redeemModel.LogoUrl = response.included.coupons.Where(x => x.couponId == item.CouponId).Select(x => x.Promotion.LogoUrl).FirstOrDefault();
+                            model.Add(redeemModel);
+                        }
+                    }
+                    else
+                    {
+                        message = "No redeem coupons available.";
+                        messageType = AlertMessageType.Error;
                     }
                 }
                 else
